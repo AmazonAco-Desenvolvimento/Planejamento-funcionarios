6652f746fc5e8c1e45d2a6551170cf084b667ec9
Ricardo Victor <ricardovictor737@gmail.com>
2025-06-27 13:16:35 -0400

    Add: Table and radios to toggle

--- app/src/app/pages/customers-page/customers-page.component.ts
+++ app/src/app/pages/customers-page/customers-page.component.ts
@@ -1,11 +1,30 @@
 import { Component } from '@angular/core';
 import { StoredDataService } from '../../services/stored-data.service';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-customers-page',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './customers-page.component.html',
-  styleUrl: './customers-page.component.css'
+  styleUrl: './customers-page.component.css',
 })
 export class CustomersPageComponent {
+  items = [0, 1, 2, 3, 4, 5];
+  toggleAllItems(allinp: HTMLInputElement) {
+    allinp.checked = allinp.checked;
+    console.log(allinp.checked)
+    if (allinp.checked === true) {
+      for (let i of this.items) {
+        let elementX = document.getElementById(`${i}`) as HTMLInputElement;
+        elementX.checked = true;
+        console.log(elementX.checked);
+      }
+    } else {
+      for (let i of this.items) {
+        let elementX = document.getElementById(`${i}`) as HTMLInputElement;
+        elementX.checked = false;
+        console.log(elementX.checked);
+      }
+    }
+  }
 }
